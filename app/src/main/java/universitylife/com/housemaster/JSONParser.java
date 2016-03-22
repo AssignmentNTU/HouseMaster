1d74529731d2cb4c8be2ff5fd2158499c09d7cec
Edward Sujono <edwardsujono@yahoo.com>
2016-03-23 04:12:59 +0800

    srachV1Update

--- app/src/main/java/universitylife/com/housemaster/JSONParser.java
+++ app/src/main/java/universitylife/com/housemaster/JSONParser.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
