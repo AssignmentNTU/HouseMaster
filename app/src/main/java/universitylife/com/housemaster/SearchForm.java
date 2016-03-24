5fb1845ef3ca6c6938e636a1239807ffaeca9592
Edward Sujono <edwardsujono@yahoo.com>
2016-03-25 02:05:54 +0800

    first commit

--- app/src/main/java/universitylife/com/housemaster/SearchForm.java
+++ app/src/main/java/universitylife/com/housemaster/SearchForm.java
@@ -1,6 +1,7 @@
 package universitylife.com.housemaster;
 
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,21 +20,47 @@ import android.view.ViewGroup;
  */
 public class SearchForm extends Fragment {
 
+    private Activity activity;
+    private EditText addressText;
+    private Button buttonGPS;
+    private PlaceReviewCollect prc;
+    private ArrayList<PlaceReview> listPlaceReview;
+    private String searchContent;
 
 
     public SearchForm() {
-        // Required empty public constructor
-        getActivity().getActionBar().setTitle("Find Place");
 
+    }
+
+    public SearchForm(Activity activity,PlaceReviewCollect prc) {
+        this.activity = activity;
+        this.prc = prc;
+        listPlaceReview = prc.getPlaceReviewList();
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        View currentView = inflater.inflate(R.layout.fragment_search_form, container, false);
+        addressText = (EditText)  currentView.findViewById(R.id.search_location);
+        buttonGPS = (Button) currentView.findViewById(R.id.search_GPS_button);
+
+        //when buttonGps is clicked
+        buttonGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_search_form, container, false);
     }
 
+
+    public void doSearching(){
+
+    }
 
 }
